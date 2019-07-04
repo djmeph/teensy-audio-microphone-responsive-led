321d6d4373187695cea767c16722028160b2cfe9
Norman Witte III <norm.witte@gmail.com>
2019-07-04 02:15:12 -0400

    Add volume pot gain adjustment

--- teensy_microphone.ino
+++ teensy_microphone.ino
@@ -12,6 +12,7 @@ AudioControlSGTL5000 sgtl5000_1;
 
 #define LED_PIN 17
 #define NUM_LEDS 45
+#define ANALOG_IN A1
 CRGB leds[NUM_LEDS];
 
 void setup() {
@@ -20,13 +21,15 @@ void setup() {
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.5);
   sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
-  sgtl5000_1.micGain(36);
+  sgtl5000_1.micGain(0);
   delay(1000);
 }
 
 elapsedMillis fps;
 int peakValue = 0;
 int i = 0;
+int gainValue = 0;
+int newGain = 0;
 
 void loop() {
   if (fps > 24) {
@@ -40,6 +43,11 @@ void loop() {
       for (i = NUM_LEDS - peakValue; i < NUM_LEDS; i++) {
         leds[i] = CRGB(0,0,255);
         FastLED.show();
+      }
+      newGain = analogRead(ANALOG_IN);
+      if (newGain != gainValue) {
+        gainValue = newGain;
+        sgtl5000_1.micGain(map(gainValue, 0, 1023, 0, 63));
       }
     }
   }
